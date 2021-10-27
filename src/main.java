d6775abc22180204e951017a258d8fe54cce5bb7
Peter Chini <pdchini@gmail.com>
2021-10-27 11:27:23 +0200

    small bug

--- src/main.java
+++ src/main.java
@@ -1,23 +1,5 @@
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
-
-import automata.*;
-import automataAlgorithms.*;
-import automatonParser.AutomParser;
-import automatonParser.AutomatonBuildListener;
-import automatonParser.AutomatonLexer;
-import automatonParser.AutomatonParser;
-import automatonPrinter.*;
-import examples.ExampleCollection;
 import examples.Test;
-import examples.Translator;
 
 public class main {
 
